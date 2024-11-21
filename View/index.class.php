981410f1b7bd79b281335dddd8f6d1c816eb3120
emeliajones <156925326+emeliajones@users.noreply.github.com>
2024-11-21 11:54:57 -0500

    fix the button

--- View/index.class.php
+++ View/index.class.php
@@ -1,11 +1,11 @@
 <?php
 class Index extends View
 {
-    public function display()
+    public function display():void
     {
         $this->header();
            echo "<h1>Welcome to the Home Screen</h1>";
-           echo "<div><a href='".$BASE_URL."'/BankAccountController/all/'</a>";
+           echo "<div><a href='".BASE_URL."'/BankAccountController/all/'</a>";
            echo '<input type="submit" class="button" value="all accounts"></div>';
         $this->footer();
     }
