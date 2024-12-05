84ec48d0dc436e841896a8a4df9a559e71271f68
crosby7 <camcrosby09@gmail.com>
2024-12-05 12:36:04 -0500

    Create notice.class.php

---
+++ View/notice.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Author: Cameron Crosby
+ * Date: 12/5/2024
+ * File: notice.class.php
+ * Description: Defines the Notice class. Alerts users when a change has occurred.
+ */
+class Notice extends View
+{
+    // public function to display a message
+    public function display($msg, $controllerName): void {
+        //call the header
+        $this->header(); ?>
+        <h1>Notice</h1>
+        <p><?= $msg ?></p>
+        <div>
+            <a href='<?= BASE_URL/$controllerName ?>/all'>
+                <input type="submit" class="button" value="Go to All Accounts">
+            </a>
+        </div>
+        <?php
+        //call the footer
+        $this->footer();
+    }
+}
