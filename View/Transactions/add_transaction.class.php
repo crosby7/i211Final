176b3aaf9e0f5ab96eb550a47f6f856911f3320c
emeliajones <156925326+emeliajones@users.noreply.github.com>
2024-12-12 12:51:51 -0500

    account id

--- View/Transactions/add_transaction.class.php
+++ View/Transactions/add_transaction.class.php
@@ -18,7 +18,7 @@ class CreateTransaction extends View
         <form class="new-media" action="<?= BASE_URL ?>/Transaction/create" method="post" style="border: 1px solid #bbb; margin-top: 10px; padding: 10px;">
             <input type="hidden" name="id" value="10">
             <p><strong>Account ID</strong>:
-                <select name="transactionType" id="accountType">j
+                <select name="accountId" id="accountId">j
                     <?php
                     foreach($accountIds as $id){
                     ?>
@@ -26,14 +26,14 @@ class CreateTransaction extends View
                         <?php }?>
                 </select>
             <p><strong>Transaction Type</strong>:
-                <select name="transactionType" id="accountType">
+                <select name="transactionType" id="transactionType">
                     <option value="Deposit">I want to make a deposit</option>
                     <option value="Withdrawal">I want to make a withdrawal</option>
                 </select>
             <p><strong>Transaction Amount</strong>:
-                <input name="transaction" type="text" size="350" value="" autofocus=""></p>
-                <input type="submit" name="action" value="Create Account">
-                <a href='<?= BASE_URL ?>/BankAccount/all'>
+                <input name="amount" type="number" step = ".01" size="350" value="" autofocus=""></p>
+                <input type="submit" name="action" value="Create Transaction">
+                <a href='<?= BASE_URL ?>/BankAccout/all'>
                     <input type="button" class="button" value="Cancel">
                 </a>
         </form>
